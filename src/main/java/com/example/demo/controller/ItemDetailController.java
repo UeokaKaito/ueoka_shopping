49871c04d90e1fbda002916eab1b926ed987cc36
Kaito Ueoka <kaito.ueoka@gmail.com>
2022-05-23 17:54:52 +0900

    画像処理の変更

--- src/main/java/com/example/demo/controller/ItemDetailController.java
+++ src/main/java/com/example/demo/controller/ItemDetailController.java
@@ -1,13 +1,10 @@
 package com.example.demo.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.example.demo.entity.Items;
 import com.example.demo.repository.ItemsRepository;
 
 @Controller
@@ -16,10 +13,11 @@ public class ItemDetailController {
 	@Autowired 
 	ItemsRepository itemsRepository;
 	
-	@RequestMapping("/{name}") //items
-	public ModelAndView detail(ModelAndView mv) {//画像が出力されない
-		List<Items> itemList = itemsRepository.findAll();
-		mv.addObject("items", itemList);
+	@RequestMapping("/items/name") //items
+	public ModelAndView detail(
+			ModelAndView mv) {//画像が出力されない
+		
+		mv.addObject("items", itemsRepository.findAll());
 		mv.setViewName("itemDetail");
 		return mv;
 	}
